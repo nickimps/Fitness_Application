07e779db1d655e19262f98f897b2d3f1157b06b8
mltommas <92130846+mltommas@users.noreply.github.com>
2023-01-28 21:58:40 -0500

    Started working on activity_workouts.xml

---
+++ app/src/main/java/com/android/fitnessapplication/WorkoutsActivity.java
@@ -0,0 +1,14 @@
+package com.android.fitnessapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts);
+    }
+}
