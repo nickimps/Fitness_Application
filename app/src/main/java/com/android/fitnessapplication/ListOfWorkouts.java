f764d036b96b99f2441e872870238d84c187eeb1
Jtsang2001 <jtsang@lakeheadu.ca>
2023-03-07 22:13:29 -0500

    Updated workouts section

--- app/src/main/java/com/android/fitnessapplication/ListOfWorkouts.java
+++ app/src/main/java/com/android/fitnessapplication/ListOfWorkouts.java
@@ -2,27 +2,31 @@ package com.android.fitnessapplication;
 
 public class ListOfWorkouts {
     static String[][] listOfWorkouts = {
-            { "Man Tits Monday",    //workout name
+            { "Explosive Chest",    //workout name
                 "Chest",    // filter
-                "4",        //# sets
-                "Barbell Bench Press", "10-12", //this point down are the exercise name and reps
+                "3",        //# sets
+                "Barbell Bench Press", "6-8", //this point down are the exercise name and reps
                 "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
-                "Dumbbell Bench Press", "10-12",
+                "Chest Flies", "10-12",
+                "Cable Flies", "10-12",
+                "Chest Press", "10-12",
+                "Incline Bench Press", "6-8",
+                "Dips", "6-8",
                 "Push-Up", "15-20" },
-            { "Thirsty Traps Tuesday",
-                "chest",
-                "4",
-                "Chest Fly", "10",
-                "Dumbbell Bench Press", "4",
-                "Push-Up", "15" },
-            { "Thirsty Traps Tuesday",
-                    "chest",
+            { "Pumped Up ShoulderS",
+                "Shoulders",
+                "3",
+                "Military Press", "4-6",
+                "Push Press", "AMRAP",
+                "Dumbell Shoulder Press", "8-10",
+                "Lateral Raises", "12-15",
+                "Shrugs", "20",
+                "Cable Extensions", "12",
+                "Rear Delt Flies", "10-12",
+                "Farmers Carry", "30 seconds"
+            },
+            { "Blasting Biceps",
+                    "Biceps",
                     "4",
                     "Chest Fly", "10",
                     "Dumbbell Bench Press", "4",
