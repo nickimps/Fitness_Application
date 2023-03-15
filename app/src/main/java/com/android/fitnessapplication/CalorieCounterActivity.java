70bdebed83bf700dd454a04ea28acca43c2c0064
mltommas <92130846+mltommas@users.noreply.github.com>
2023-03-15 17:00:29 -0400

    added button functionality to go from CalorieCounterActivity to RecordYourMealActivity

--- app/src/main/java/com/android/fitnessapplication/CalorieCounterActivity.java
+++ app/src/main/java/com/android/fitnessapplication/CalorieCounterActivity.java
@@ -1,8 +1,12 @@
 package com.android.fitnessapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalorieCounterActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class CalorieCounterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calorie_counter);
+
+        CardView recordMealCardView = findViewById(R.id.recordMealCardView);
+        recordMealCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Go to register screen on click
+                Intent intent = new Intent(CalorieCounterActivity.this, RecordYourMealActivity.class);
+                startActivity((intent));
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/android/fitnessapplication/RecordYourMealActivity.java
+++ app/src/main/java/com/android/fitnessapplication/RecordYourMealActivity.java
@@ -2,7 +2,9 @@ package com.android.fitnessapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecordYourMealActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class RecordYourMealActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_your_meal);
     }
+
+    public class MyOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(RecordYourMealActivity.this, RecordYourMealActivity.class);
+            startActivity((intent));
+        }
+    }
+
 }
