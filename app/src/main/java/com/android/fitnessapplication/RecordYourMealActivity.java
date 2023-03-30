d301c1b1cbf01b3e70016c8fa97fe7ff4ec62177
Jtsang2001 <jtsang@lakeheadu.ca>
2023-03-29 23:39:23 -0400

    Updated Calorie Counter Screen, added shared preferences so data saves when app is closed

--- app/src/main/java/com/android/fitnessapplication/RecordYourMealActivity.java
+++ app/src/main/java/com/android/fitnessapplication/RecordYourMealActivity.java
@@ -19,6 +19,7 @@ public class RecordYourMealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_your_meal);
+
 //test
         Intent mealIntent = getIntent();
         Bundle extra = new Bundle();
